ec8c6de4302285536727bbb405a1b1e1b46160ec
Farhan Jailani <farhanjailani@gmail.com>
2017-08-24 12:14:49 +0530

    Add files via upload

--- Authentication.py
+++ Authentication.py
@@ -1,9 +1,7 @@
-fp=open('Employee.txt','r')
-n=int(fp.readline())
-employee={}
-for i in range(n):
-    strs=(fp.readline()).split()
-    employee[strs[0]]=[strs[1],int(strs[2])]
+import shelve
+f1=shelve.open('empdata.txt','c')
+employee=f1['emp']
+f1.close()
 def Authenticate(username,password,n):
     if(employee[username][0]==password and employee[username][1]==n):
         return True
